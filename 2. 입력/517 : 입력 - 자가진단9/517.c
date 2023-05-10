5b8b85d29c2a06734c09dc4e4e4543bdb297886c
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-05-11 01:14:47 +0900

    Create 517.c

---
+++ 2. 입력/517 : 입력 - 자가진단9/517.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    double a, b, c;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    printf("%.3lf\n", a);
+    printf("%.3lf\n", b);
+    printf("%.3lf\n", c);
+    
+    return 0;
+}
