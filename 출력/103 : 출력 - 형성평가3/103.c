3fb01011277769d695cc4795e2427356f558da49
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-05-10 23:13:40 +0900

    Update 103.c

--- 출력/103 : 출력 - 형성평가3/103.c
+++ 출력/103 : 출력 - 형성평가3/103.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main(void) {
+    printf("TTTTTTTTTT\n");
+    printf("TTTTTTTTTT\n");
+    printf("    TT    \n");
+    printf("    TT    \n");
+    printf("    TT    \n");
+
+    return 0;
+}
