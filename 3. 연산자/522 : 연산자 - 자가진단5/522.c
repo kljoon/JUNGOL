4442c02b18dd9a59d7d0e795e965bbdd8d85964b
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-05-11 18:43:04 +0900

    Create 522.c

---
+++ 3. 연산자/522 : 연산자 - 자가진단5/522.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    int result1, result2;
+
+    scanf("%d %d", &a, &b);
+
+    result1 = a == b;
+    result2 = a != b;
+
+    printf("%d\n", result1);
+    printf("%d\n", result2);
+
+    return 0;
+}
