040984356959aefb66e4eeacf96cd9c6b9262dc2
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-09-23 23:26:33 +0900

    Create 506.c

---
+++ 문제집/단계별 문제/LCoder_C언어/출력 - 자가진단6/506.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%s \n", "My height");
+    printf("%d \n", 170);
+    printf("%s \n", "My weight");
+    printf("%f \n", 68.6);
+
+    return 0;
+}
