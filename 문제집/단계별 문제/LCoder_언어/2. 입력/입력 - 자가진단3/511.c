87f789fd9bfaab38823a5602df26324b8180f103
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-09-24 22:47:28 +0900

    Create 511.c

---
+++ 문제집/단계별 문제/LCoder_언어/2. 입력/입력 - 자가진단3/511.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+
+    a = 55;
+    b = 10;
+
+    printf("%d - %d = %d\n", a, b, a - b);
+
+    a = 2008;
+    b = 1999;
+
+    printf("%d - %d = %d\n", a, b, a - b);
+
+    return 0;
+}
