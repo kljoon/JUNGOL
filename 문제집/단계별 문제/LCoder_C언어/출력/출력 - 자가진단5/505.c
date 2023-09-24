32c839505acc32dfa4e88feeff86d92d2533ed5f
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-09-24 14:27:46 +0900

    Delete 문제집/단계별 문제/LCoder_C언어 directory

--- 문제집/단계별 문제/LCoder_C언어/출력/출력 - 자가진단5/505.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("%s\n", "I can program well.");
-    printf("%s\n", "Dreams come true.");
-
-    return 0;
-}
--- 문제집/단계별 문제/LCoder_C언어/출력/출력 - 자가진단7/507.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("%d Dan\n", 5);
-    printf("%d * %d = %d\n", 5, 2, 5 * 2);
-
-    return 0;
-}
