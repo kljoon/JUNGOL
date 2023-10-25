93289fc196f676916f6a2cd498496c0ab51c2e02
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-10-25 23:51:28 +0900

    Create 9020.c

---
+++ 문제집/단계별 문제/LCoder_언어/3. 연산자/연산자 - 연습문제2-2/9020.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d, e;
+
+    printf("5개의 수를 입력하시오. ");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+    a = a + 3;
+    b = b - 3;
+    c = c * 3;
+    d = d / 3;
+    e = e % 3;
+
+    printf("%d %d %d %d %d\n", a, b, c, d, e);
+
+    return 0;
+}
