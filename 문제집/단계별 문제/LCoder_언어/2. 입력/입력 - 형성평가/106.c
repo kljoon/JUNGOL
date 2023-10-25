5ee9e51a7b8f89946b1d517207ef4839bce8229b
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-10-25 22:44:26 +0900

    Create 106.c

---
+++ 문제집/단계별 문제/LCoder_언어/2. 입력/입력 - 형성평가/106.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    int c;
+
+    a = 10;
+    b = 20;
+    c = 30;
+
+    printf("%d + %d = %d\n", a, b, a + b);
+
+    return 0;
+}
