9444ee72536f407e6cb3a416b68734bd82738602
KYUNGMIN LEE <117913703+kljoon@users.noreply.github.com>
2023-10-25 22:39:43 +0900

    Create 517.c

---
+++ 문제집/단계별 문제/LCoder_언어/2. 입력/입력 - 자가진단9/517.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double a;
+    double b;
+    double c;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    printf("%.3lf\n", a);
+    printf("%.3lf\n", b);
+    printf("%.3lf\n", c);
+
+    return 0;
+}
